b0dcc9d3ec01e47b894e7a313cdd64ba55fbea82
alifzulkifeli <alifzulkifeli@gmail.com>
2024-04-30 18:45:28 +0800

    asd

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,157 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+import { useEffect, useState } from 'react';
+import { Joystick } from 'react-joystick-component';
+import data from './data.json';
+import toast, { Toaster } from 'react-hot-toast';
+
 
 export default function Home() {
+  const notifySucces = () => toast.success('+1');
+  const notifyError = () => toast.error('-1');
+
+
+  const [direction, setDirection] = useState('')
+  const [Q, setQ] = useState([])
+  const [shuffled, setShuffled] = useState([])
+
+  const [styleOnOne, setStyleOnOne] = useState('')
+  const [styleOnTwo, setStyleOnTwo] = useState('')
+  const [styleOnThree, setStyleOnThree] = useState('')
+  const [styleOnFour, setStyleOnFour] = useState('')
+
+  const [score, setScore] = useState(0)
+
+  const effect = "text-blue-600 font-bold text-4xl"
+
+
+  const getData = () => {
+    const randomData1 = data[Math.floor(Math.random() * data.length)];
+    const randomData2 = data[Math.floor(Math.random() * data.length)];
+    const randomData3 = data[Math.floor(Math.random() * data.length)];
+    const randomData4 = data[Math.floor(Math.random() * data.length)];
+
+    setQ([randomData1, randomData2, randomData3, randomData4])
+    console.log(randomData1);
+    // shuffle the array of 4 elements of 0,1,2,3
+    setShuffled([0, 1, 2, 3].sort(() => Math.random() - 0.5))
+  }
+
+  useEffect(() => {
+   getData()
+  }, [])
+
+  const handleMove = (e) => {
+    setDirection(e.direction);
+    if (e.direction === 'FORWARD') {
+      setStyleOnOne(effect);
+      setStyleOnTwo('');
+      setStyleOnThree('');
+      setStyleOnFour('');
+    }
+    if (e.direction === 'LEFT') {
+      setStyleOnOne('');
+      setStyleOnTwo(effect);
+      setStyleOnThree('');
+      setStyleOnFour('');
+    }
+    if (e.direction === 'RIGHT') {
+      setStyleOnOne('');
+      setStyleOnTwo('');
+      setStyleOnThree(effect);
+      setStyleOnFour('');
+    }
+    if (e.direction === 'BACKWARD') {
+      setStyleOnOne('');
+      setStyleOnTwo('');
+      setStyleOnThree('');
+      setStyleOnFour(effect);
+    }
+  }
+
+  const reset   = () => {
+    setStyleOnOne('')
+    setStyleOnTwo('')
+    setStyleOnThree('')
+    setStyleOnFour('')
+    getData()
+
+
+  }
+
+  const OK = () => {
+    notifySucces()
+    setScore(score + 1)
+    reset()
+  }
+
+  const NG = () => {
+    notifyError()
+    setScore(score - 1)
+    reset()
+  }
+
+  const handleStop = (e) => {
+    console.log(direction, Q[0].id, Q[shuffled[0]], Q[shuffled[1]].id, Q[shuffled[2]].id, Q[shuffled[3]].id);
+    if (direction === 'FORWARD') {
+      if (Q[shuffled[0]].no === Q[0].no) OK()
+      else NG()
+    }
+    if (direction === 'RIGHT') {
+      if (Q[shuffled[1]].no === Q[0].no) OK()
+      else NG()
+    }
+    if (direction === 'BACKWARD') {
+      if (Q[shuffled[2]].no === Q[0].no) OK()
+      else NG()
+    }
+    if (direction === 'LEFT') {
+      if (Q[shuffled[3]].no === Q[0].no) OK()
+      else NG()
+    }
+  }
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between w-screen `}
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div className="flex flex-col items-center w-screen ">
+        <div className=" flex place-items-center ">
+          <div className="flex flex-col items-center">
+            <h1 className="text-4xl font-bold text-center mt-40">{Q[0]?.kanji}</h1>
+            <h2 className="text-2xl font-semibold text-center">{Q[0]?.reading}</h2>
+          </div>
         </div>
+
+        <div className={`pt-20 text-xl ${styleOnOne}`} >
+          {Q[shuffled[0]]?.definition}
+        </div>
+        <div className="flex flex-row items-center w-screen">
+          <div className={`w-full  text-xl text-right ${styleOnTwo}`} >
+            {Q[shuffled[3]]?.definition}
+          </div>
+          <div className='p-10 ' >
+            <Joystick move={handleMove} stop={handleStop} />
+          </div>
+          <div className={`w-full  text-xl ${styleOnThree}`} >
+            {Q[shuffled[1]]?.definition}
+          </div>
+        </div>
+        <div className={` text-xl ${styleOnFour}`} >
+          {Q[shuffled[2]]?.definition}
+        </div>
+
+        <div className="flex flex-col items-center w-screen">
+        <h1 className="text-4xl font-bold text-center mt-20">Score: {score}</h1>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
       </div>
+      <Toaster
+        position="top-right"
+        reverseOrder={true}
+      />
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+   
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   );
 }
+
+
